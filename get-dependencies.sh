631aa93c45ed2eb125b428e0fd29326810448378
juju4 <juju4@users.noreply.github.com>
2017-04-30 17:24:07 -0400

    'Beginning with IPython 6.0, Python 3.3 and above is required.' for Xenial and Centos7

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,6 +9,7 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 [ ! -d $rolesdir/juju4.vivisect ] && git clone https://github.com/juju4/ansible-vivisect $rolesdir/juju4.vivisect
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.floss ] && ln -s ansible-floss $rolesdir/juju4.floss
